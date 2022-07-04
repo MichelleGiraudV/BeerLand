800be0842f7542768329c877a0bdca6cf0b1353c
Michelle <michellegiraudv@gmail.com>
2022-07-04 11:31:52 -0500

    beerlanddatabase

---
+++ BeerLandDatabase.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-07-2022 a las 18:28:50
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `beerland`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `order_id`
+--
+
+CREATE TABLE `order_id` (
+  `order_id` int(11) NOT NULL,
+  `category` varchar(20) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Significado de cada una de las categorías del order_id';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `product_id` int(11) NOT NULL,
+  `prod_name` varchar(20) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
+  `image` varchar(40) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `category` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Tabla para registrar los productos';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `product_category`
+--
+
+CREATE TABLE `product_category` (
+  `category_id` varchar(10) NOT NULL,
+  `category` varchar(20) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sales_detail`
+--
+
+CREATE TABLE `sales_detail` (
+  `user_id` int(11) NOT NULL,
+  `id_sale_detail` int(11) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `num_ticket` int(11) NOT NULL,
+  `total` decimal(10,0) NOT NULL,
+  `tax` decimal(10,0) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Tabla para registrar los detalles de las ventas';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `user_id` int(10) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `image` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Tabla para registrar los usuarios y sus datos';
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `order_id`
+--
+ALTER TABLE `order_id`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`category`),
+  ADD UNIQUE KEY `product_id` (`product_id`);
+
+--
+-- Indices de la tabla `product_category`
+--
+ALTER TABLE `product_category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indices de la tabla `sales_detail`
+--
+ALTER TABLE `sales_detail`
+  ADD PRIMARY KEY (`order_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`),
+  ADD UNIQUE KEY `product_id` (`product_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `sales_detail` (`product_id`);
+
+--
+-- Filtros para la tabla `product_category`
+--
+ALTER TABLE `product_category`
+  ADD CONSTRAINT `product_category_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `productos` (`category`);
+
+--
+-- Filtros para la tabla `sales_detail`
+--
+ALTER TABLE `sales_detail`
+  ADD CONSTRAINT `sales_detail_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order_id` (`order_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `sales_detail_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `usuarios` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
